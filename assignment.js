3323df92fc580f220a30a182b9807957025ea79a
Albee89 <137462408+Albee89@users.noreply.github.com>
2023-08-15 23:37:31 +1000

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,16 +1,51 @@
 
+fadeIn();
+
+          window.onload=function() {
+              var displayArea = document.getElementsByClassName("display-area");
+              var btn = document.getElementsByClassName("submit-btn");
+              btn.onclick = function() {
+                  var comment = document.getElementsByClassName("message").value;
+                  displayArea.appendChild(comment);
+              };
+          }  
+          function hideIconBar(){
+            var iconBar = document.getElementById("iconBar");
+            var navigation = document.getElementById("navigation");
+            iconBar.setAttribute("style", "display:none;");
+            navigation.classList.remove("hide");
+        }
+        
+        function showIconBar(){
+            var iconBar = document.getElementById("iconBar");
+            var navigation = document.getElementById("navigation");
+            iconBar.setAttribute("style", "display:block;");
+            navigation.classList.add("hide");
+        }
+        
+        //Comment
+        function showComment(){
+            var commentArea = document.getElementById("comment-area");
+            commentArea.classList.remove("hide");
+        }
+        
+        //Reply
+        function showReply(){
+            var replyArea = document.getElementById("reply-area");
+            replyArea.classList.remove("hide");
+        }
+
 const carousel = document.querySelector('.img-carousel');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 let carsouselImages = document.querySelectorAll('.img-carousel div');
-
 
 //Next Carousel
 const nextCarousel = () => {
     if(carsouselImages[carsouselImages.length - 1]) {
       carousel.scrollTo(0, 0); 
     } 
-      carousel.scrollBy(300, 0);
+      carousel.scrollBy(400, 0);
 };
 
 nextBtn.addEventListener('click', e => {
@@ -22,12 +57,13 @@ const prevCarousel = () => {
    if(carsouselImages[0]) {
     carousel.scrollTo(4800,0);
    }
-    carousel.scrollBy(-300, 0); 
+    carousel.scrollBy(-400, 0); 
 };
 
 prevBtn.addEventListener('click', e => {
    prevCarousel(); 
 });
+
 
 
 // Auto carousel
@@ -71,3 +107,30 @@ window.addEventListener('resize', () => {
     }, 200);
 }); 
 
+var list = [];
+
+if (list.length == 0) {
+  $(".list-group").visible = false;
+}
+
+var moduleA = angular.module("ModuleA", []);
+
+moduleA.controller("MsgController", function($scope) {
+  $scope.Message = "測試一下";
+  $scope.User = "World";
+  $scope.response = { msgList: list };
+  $scope.newMsg = function() {
+    //console.log($("#txtNewMsg").val());
+    list.push($("#txtNewMsg").val());
+    $("#txtNewMsg").val("");
+    //console.log(list);
+  };
+
+  $scope.deleteMsg = function(index) {
+    list.splice(index, 1);
+  };
+});
+
+moduleA.controller("UserController", function($scope) {});
+
+ 
