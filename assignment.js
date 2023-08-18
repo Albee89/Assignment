96b9d811bec0d4247b26b097498404f7fc40cf2b
Albee89 <137462408+Albee89@users.noreply.github.com>
2023-08-18 16:56:36 +1000

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,13 +1,5 @@
 
 
-const carousel = document.querySelector('.img-carousel');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-let carsouselImages = document.querySelectorAll('.img-carousel div');
-
-/
-
-
 // Auto carousel
 const auto = true; // Auto scroll
 const intervalTime = 5000;
@@ -26,14 +18,14 @@ carousel.addEventListener('mouseleave', (startInterval) => {
   if (auto) {
     sliderInterval = setInterval(nextCarousel, intervalTime);
   }
-}); 
+});
 
 //for mobile events
 carousel.addEventListener('touchstart', (stopIntervalT) => {
-    clearInterval(sliderInterval);
+  clearInterval(sliderInterval);
 });
 carousel.addEventListener('touchend', (startIntervalT) => {
-   if (auto) {
+  if (auto) {
     sliderInterval = setInterval(nextCarousel, intervalTime);
   }
 });
@@ -41,13 +33,13 @@ carousel.addEventListener('touchend', (startIntervalT) => {
 //Debounce
 var previousCall;
 window.addEventListener('resize', () => {
-    if (previousCall >= 0) {
-        clearTimeout(previousCall);
-    } 
-    previousCall = setTimeout(() => {
-      carousel.scrollBy(-300, 0); 
-    }, 200);
-}); 
+  if (previousCall >= 0) {
+    clearTimeout(previousCall);
+  }
+  previousCall = setTimeout(() => {
+    carousel.scrollBy(-300, 0);
+  }, 200);
+});
 
 var list = [];
 
@@ -57,22 +49,84 @@ if (list.length == 0) {
 
 var moduleA = angular.module("ModuleA", []);
 
-moduleA.controller("MsgController", function($scope) {
+moduleA.controller("MsgController", function ($scope) {
   $scope.Message = "測試一下";
   $scope.User = "World";
   $scope.response = { msgList: list };
-  $scope.newMsg = function() {
+  $scope.newMsg = function () {
     //console.log($("#txtNewMsg").val());
     list.push($("#txtNewMsg").val());
     $("#txtNewMsg").val("");
     //console.log(list);
   };
 
-  $scope.deleteMsg = function(index) {
+  $scope.deleteMsg = function (index) {
     list.splice(index, 1);
   };
 });
 
-moduleA.controller("UserController", function($scope) {});
+moduleA.controller("UserController", function ($scope) { });
 
- 
+
+// IMAGE GALLERY  //
+
+const slides = document.querySelectorAll(".slide");
+
+
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+let curSlide = 0;
+
+// select next slide button
+const nextSlide = document.querySelector(".btn-next");
+
+// add event listener and next slide functionality
+nextSlide.addEventListener("click", function () {
+     curSlide++;
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+const nextSlide = document.querySelector(".btn-next");
+
+
+let curSlide = 0;
+
+let maxSlide = slides.length - 1;
+
+
+nextSlide.addEventListener("click", function () {
+  
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+const prevSlide = document.querySelector(".btn-prev");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+  // check if current slide is the first and reset current slide to last
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide-;
+  }
+
+  //   move slide by 100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
