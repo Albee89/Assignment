757acaaafc85213881b5201cc29db3e956d0a908
Albee89 <137462408+Albee89@users.noreply.github.com>
2023-08-28 11:34:47 +1000

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -23,9 +23,78 @@ function validate() {
   }
 }
 
+///////NEWS CAROUSEL////
+
+const carousel = document.querySelector('.img-carousel');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+let carsouselImages = document.querySelectorAll('.img-carousel div');
+
+//Next Carousel
+const nextCarousel = () => {
+    if(carsouselImages[carsouselImages.length - 1]) {
+      carousel.scrollTo(0, 0); 
+    } 
+      carousel.scrollBy(300, 0);
+};
+
+nextBtn.addEventListener('click', e => {
+  nextCarousel(); 
+});
+
+//Prev Carousel
+const prevCarousel = () => {
+   if(carsouselImages[0]) {
+    carousel.scrollTo(4800,0);
+   }
+    carousel.scrollBy(-300, 0); 
+};
+
+prevBtn.addEventListener('click', e => {
+   prevCarousel(); 
+});
 
 
+// Auto carousel
+const auto = true; // Auto scroll
+const intervalTime = 5000;
+let sliderInterval;
 
+
+if (auto) {
+  sliderInterval = setInterval(nextCarousel, intervalTime);
+};
+
+carousel.addEventListener('mouseover', (stopInterval) => {
+  clearInterval(sliderInterval);
+});
+
+carousel.addEventListener('mouseleave', (startInterval) => {
+  if (auto) {
+    sliderInterval = setInterval(nextCarousel, intervalTime);
+  }
+}); 
+
+//for mobile events
+carousel.addEventListener('touchstart', (stopIntervalT) => {
+    clearInterval(sliderInterval);
+});
+carousel.addEventListener('touchend', (startIntervalT) => {
+   if (auto) {
+    sliderInterval = setInterval(nextCarousel, intervalTime);
+  }
+});
+
+//Debounce
+var previousCall;
+window.addEventListener('resize', () => {
+    if (previousCall >= 0) {
+        clearTimeout(previousCall);
+    } 
+    previousCall = setTimeout(() => {
+      carousel.scrollBy(-300, 0); 
+    }, 200);
+}); 
 // Contact Us ////////////
 
 const contact = document.createElement("p");
@@ -227,4 +296,19 @@ function myfunction()
         document.getElementById("displaytable").style.display="block";
     else
         document.getElementById("displaytable").style.display="none";
+}
+
+////CANVAS DRAWING////
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+function sun() {
+  ctx.strokeStyle = 'Yellow';
+  ctx.fillStyle = 'rgba(255,255,0,0.1)';
+  ctx.lineWidth = 5;
+
+  ctx.beginPath();
+  ctx.arc(100,100, 50, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
 }
